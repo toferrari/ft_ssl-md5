bba5a7fd9ce736c16e1da1fa69468c662cd9b1b6
Tony FERRARI <tferrari@e2r4p19.42.fr>
2019-02-25 17:46:16 +0100

    update for new file c

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 13:50:32 by tferrari          #+#    #+#             */
-/*   Updated: 2019/02/24 16:35:36 by tferrari         ###   ########.fr       */
+/*   Updated: 2019/02/25 17:19:51 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,18 @@
 # include "libft.h"
 # include "ft_printf.h"
 # include "get_next_line.h"
+# include <fcntl.h>
+
+# define BUFSIZE 500
+
+void	md5(void* input_to_h, size_t len_h);
+
+typedef struct		s_option
+{
+	int				option_p;
+	int				option_q;
+	int				option_r;
+	int				option_s;
+}					t_option;
 
 #endif
