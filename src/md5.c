0ebf5460b40bc40a751613f36d1c1087fd7600b0
Tony FERRARI <tferrari@e2r4p19.42.fr>
2019-02-25 17:58:15 +0100

    check space for block 512 need update for 64

--- src/md5.c
+++ src/md5.c
@@ -6,14 +6,22 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 16:08:06 by tferrari          #+#    #+#             */
-/*   Updated: 2019/02/24 16:53:58 by tferrari         ###   ########.fr       */
+/*   Updated: 2019/02/25 17:40:40 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-void	md5(char *str)
+void	md5(void* input_to_h, size_t len_h)
 {
-	len_str = ft_strlen(str);
-	if ()
+	int		mod_h;
+	void	*input_to_h_mod;
+
+	mod_h = len_h % 512;
+	if (mod_h != 0)
+	{
+		input_to_h_mod = ft_realloc_md5(input_to_h, len_h - mod_h + 512, len_h);
+		ft_memdel(&input_to_h);
+	}
+	
 }
