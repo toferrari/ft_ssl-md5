5b033cd84e002ac6d29eb974dd047ef6fbd6de27
Tony FERRARI <tferrari@e3r8p9.42.fr>
2019-02-26 19:21:57 +0100

    add struct md5

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 13:50:32 by tferrari          #+#    #+#             */
-/*   Updated: 2019/02/25 17:19:51 by tferrari         ###   ########.fr       */
+/*   Updated: 2019/02/26 18:20:33 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@
 # include "ft_printf.h"
 # include "get_next_line.h"
 # include <fcntl.h>
+# include <math.h>
 
 # define BUFSIZE 500
-
-void	md5(void* input_to_h, size_t len_h);
 
 typedef struct		s_option
 {
@@ -29,5 +28,23 @@ typedef struct		s_option
 	int				option_r;
 	int				option_s;
 }					t_option;
+
+typedef struct		s_md5
+{
+	int				fd;
+	int				ret;
+	char			buf[BUFSIZE];
+	void			*data_to_h;
+	size_t			len_octet_h;
+	size_t			nb_block;
+	uint32_t		h_init[4];
+	uint32_t		h_update[4];
+	uint32_t		*r;
+	uint32_t		*k;
+	uint32_t		f;
+	uint32_t		g;
+}					t_md5;
+
+void				ft_md5(t_md5 md5);
 
 #endif
