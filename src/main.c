47b647277acd8a5c36dc3300045a0819b23d37c3
Tony FERRARI <tferrari@e3r8p9.42.fr>
2019-02-26 19:22:28 +0100

    update open read for struct md5

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 13:59:57 by tferrari          #+#    #+#             */
-/*   Updated: 2019/02/25 16:35:25 by tferrari         ###   ########.fr       */
+/*   Updated: 2019/02/26 19:21:23 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-int error(char *str)
+int	error(char *str)
 {
 	ft_putstr(str);
 	return (0);
@@ -21,24 +21,20 @@ int error(char *str)
 int	main(int ac, char **av)
 {
 	t_option	option;
-	char		buf[BUFSIZE];
-	void		*ret_buf;
-	int			fd;
-	int			ret;
+	t_md5		md5;
 
 	if (ac == 1)
 		return error(("usage: ft_ssl command [command opts] [command args]"));
-
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-		error("error fd");
-	int total_ret = 0;
-	while ((ret = read(fd, buf, BUFSIZE)) > 0)
+	ft_bzero(&md5, sizeof(md5));
+	md5.fd = open(av[1], O_RDONLY);
+	if (md5.fd == -1)
+		error("error md5.fd");
+	while ((md5.ret = read(md5.fd, md5.buf, BUFSIZE)) > 0)
 	{
-		ret_buf = ft_realloc_md5(ret_buf, total_ret + ret, total_ret);
-		ret_buf = ft_memccat(ret_buf, buf, total_ret, ret);
-		total_ret += ret;
+		md5.data_to_h = ft_realloc_md5(md5.data_to_h, md5.len_octet_h + md5.ret, md5.len_octet_h);
+		md5.data_to_h = ft_memccat(md5.data_to_h, md5.buf, md5.len_octet_h, md5.ret);
+		md5.len_octet_h += md5.ret;
 	}
-	md5(ret_buf, total_ret);
+	// md5(md5.data_to_h, md5.len_octet_h);
 	return (0);
 }
