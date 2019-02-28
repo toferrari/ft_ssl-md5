b82ee521b4d1bfc80dbbc1bf4c59731f1bcce360
Tony FERRARI <tferrari@e2r4p19.42.fr>
2019-02-28 18:40:15 +0100

    algo fixed, next step fct padding

--- src/md5.c
+++ src/md5.c
@@ -6,7 +6,7 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 16:08:06 by tferrari          #+#    #+#             */
-/*   Updated: 2019/02/26 19:07:07 by tferrari         ###   ########.fr       */
+/*   Updated: 2019/02/28 18:33:08 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static uint32_t	g_r[64] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12,
 	23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15, 21, 6, 10, 15,
 	21, 6, 10, 15, 21, 6, 10, 15, 21};
 
-static uint32_t g_k[64] = {	0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+static uint32_t g_k[64] = { 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
 	0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af,
 	0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
 	0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453,
@@ -27,7 +27,7 @@ static uint32_t g_k[64] = {	0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
 	0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5,
 	0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
 	0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0,
-	0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,};
+	0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
 t_md5	init_variable_h(t_md5 md5)
 {
@@ -40,30 +40,48 @@ t_md5	init_variable_h(t_md5 md5)
 	return (md5);
 }
 
-t_md5	init_update_h(t_md5 md5)
+t_md5	compute(t_md5 md5, uint32_t *w, size_t i)
 {
-	md5.h_update[0] = md5.h_init[0];
-	md5.h_update[1] = md5.h_init[1];
-	md5.h_update[2] = md5.h_init[2];
-	md5.h_update[3] = md5.h_init[3];
+	if (i <= 15)
+	{
+		md5.f = (md5.h_update[1] & md5.h_update[2]) |
+										((~md5.h_update[1]) & md5.h_update[3]);
+		md5.g = i;
+	}
+	else if (i <= 31)
+	{
+		md5.f = (md5.h_update[3] & md5.h_update[1]) |
+										((~md5.h_update[3]) & md5.h_update[2]);
+		md5.g = (5 * i + 1) % 16;
+	}
+	else if (i <= 47)
+	{
+		md5.f = md5.h_update[1] ^ md5.h_update[2] ^ md5.h_update[3];
+		md5.g = (3 * i + 5) % 16;
+	}
+	else
+	{
+		md5.f = md5.h_update[2] ^ (md5.h_update[1] | ~md5.h_update[3]);
+		md5.g = (7 * i) % 16;
+	}
 	return (md5);
 }
 
-t_md5	update_h_update(size_t i, t_md5 md5)
+t_md5	calculate_h(t_md5 md5, uint32_t *w)
 {
-	size_t tmp;
-	int *w;
+	size_t	i;
 
-	tmp = md5.h_update[3];
-	md5.h_update[2] = md5.h_update[2];
-	md5.h_update[1] = ((md5.h_update[0] + md5.f + md5.k[i] + w[md5.g]) <<
-													md5.r[i]) + md5.h_update[2];
-	md5.h_update[0] = tmp;
-	return (md5);
-}
-
-t_md5	update_h(t_md5 md5)
-{
+	i = 0;
+	while (i < 64)
+	{
+		md5 = compute(md5, w, i);
+		md5.f = md5.f + md5.h_update[0] + md5.k[i] + w[md5.g];
+		md5.h_update[0] = md5.h_update[3];
+		md5.h_update[3] = md5.h_update[2];
+		md5.h_update[2] = md5.h_update[1];
+		md5.h_update[1] = md5.h_update[1] + L_ROT(md5.f, md5.r[i]);
+		i++;
+	}
 	md5.h_init[0] += md5.h_update[0];
 	md5.h_init[1] += md5.h_update[1];
 	md5.h_init[2] += md5.h_update[2];
@@ -71,40 +89,9 @@ t_md5	update_h(t_md5 md5)
 	return (md5);
 }
 
-t_md5	calculate_h(t_md5 md5)
+t_md5	create_padding(t_md5 md5)
 {
-	size_t	i;
-	size_t	f;
-	size_t	g;
 
-	i = 0;
-	while (i < 64)
-	{
-		if (i <= 15)
-		{
-			md5.f = (md5.h_update[1] & md5.h_update[2]) |
-											((~md5.h_update[1]) & md5.h_update[3]);
-			md5.g = i;
-		}
-		else if (i > 15 && i <= 31)
-		{
-			md5.f = (md5.h_update[3] & md5.h_update[1]) |
-											((~md5.h_update[3]) & md5.h_update[2]);
-			md5.g = (5 * i + 1) % 16;
-		}
-		else if (i > 31 && i <= 47)
-		{
-			md5.f = md5.h_update[1] ^ md5.h_update[2] ^ md5.h_update[3];
-			md5.g = (3 * i + 5) % 16;
-		}
-		else if (i > 47 && i <= 63)
-		{
-			md5.f = md5.h_update[2] ^ (md5.h_update[1] | ~md5.h_update[3]);
-			md5.g = (7 * i) % 16;
-		}
-		md5 = update_h_update(i, md5);
-		i++;
-	}
 	return (md5);
 }
 
@@ -112,24 +99,28 @@ void	ft_md5(t_md5 md5)
 {
 	size_t		mod_h;
 	size_t		loop;
+	size_t		bytes_allocate;
+	void		*ptr_save;
+	uint32_t	*test;
 
 	mod_h = md5.len_octet_h % 64;
 	md5 = init_variable_h(md5);
-	if (mod_h != 0)
-	{
-		md5.data_to_h = ft_realloc_md5(md5.data_to_h,
+	md5.data_to_h = ft_realloc_md5(md5.data_to_h,
 								md5.len_octet_h - mod_h + 64, md5.len_octet_h);
-		((char *)md5.data_to_h)[md5.len_octet_h] = 1;
-		md5.len_octet_h += (64 - mod_h);
-		// write(1, md5.data_to_h, md5.len_octet_h);
-	}
-	md5.nb_block = md5.len_octet_h / 64;
-	loop = 0;
-	while (loop < md5.nb_block)
+	((char *)md5.data_to_h)[md5.len_octet_h] = 0x80;
+	md5.len_octet_h += (64 - mod_h);
+	((char *)md5.data_to_h)[md5.len_octet_h - 8] = 96;
+	write(1, md5.data_to_h, md5.len_octet_h);
+	write(1, "\n", 1);
+	test = (uint32_t*)md5.data_to_h;
+	md5.nb_block = md5.len_octet_h;
+	ptr_save = md5.data_to_h;
+	while (0 < md5.len_octet_h)
 	{
-		md5 = init_update_h(md5);
-		md5 = calculate_h(md5);
-		md5 = update_h(md5);
-		loop++;
+		ft_memcpy(md5.h_update, md5.h_init, sizeof(md5.h_init));
+		md5 = calculate_h(md5, test);
+		md5.len_octet_h -= 64;
+		test = test + 16;
 	}
+	ft_printf("%x%x%x%x\n",md5.h_init[0], md5.h_init[1], md5.h_init[2], md5.h_init[3]);
 }
