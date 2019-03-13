2724fff7fb97d6550289d6cbbeb7ebe38f7608ae
Tony FERRARI <tferrari@e3r4p20.42.fr>
2019-03-13 19:09:39 +0100

    algo done

--- src/sha256.c
+++ src/sha256.c
@@ -6,7 +6,7 @@
 /*   By: tferrari <tferrari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 10:59:01 by tferrari          #+#    #+#             */
-/*   Updated: 2019/03/07 18:42:54 by tferrari         ###   ########.fr       */
+/*   Updated: 2019/03/13 19:03:55 by tferrari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,99 @@ t_hash	init_variable_h_sha256(t_hash hash)
 	return (hash);
 }
 
+t_hash	compute_sha256(t_hash hash, size_t i, uint32_t *w)
+{
+	uint32_t s1;
+	uint32_t ch;
+	uint32_t tmp;
+	uint32_t s0;
+	uint32_t maj;
+
+	s1 = R_ROT(hash.h_update[4], 6) ^ R_ROT(hash.h_update[4], 11) ^ \
+													R_ROT(hash.h_update[4], 25);
+	ch = (hash.h_update[4] & hash.h_update[5]) ^ (~hash.h_update[4] & \
+															hash.h_update[6]);
+	tmp = hash.h_update[7] + s1 + ch + g_k[i] + w[i];
+	s0 = R_ROT(hash.h_update[0], 2) ^ R_ROT(hash.h_update[0], 13) ^ \
+													R_ROT(hash.h_update[0], 22);
+	maj = (hash.h_update[0] & hash.h_update[1]) ^ (hash.h_update[0] & \
+					hash.h_update[2]) ^ (hash.h_update[1] & hash.h_update[2]);
+	hash.h_update[7] = hash.h_update[6];
+	hash.h_update[6] = hash.h_update[5];
+	hash.h_update[5] = hash.h_update[4];
+	hash.h_update[4] = hash.h_update[3] + tmp;
+	hash.h_update[3] = hash.h_update[2];
+	hash.h_update[2] = hash.h_update[1];
+	hash.h_update[1] = hash.h_update[0];
+	hash.h_update[0] = tmp + s0 + maj;
+	return (hash);
+}
+
+t_hash	calculate_h_sha256(t_hash hash, uint8_t *m)
+{
+	int			i;
+	size_t		j;
+	uint32_t	w[64];
+
+	i = -1;
+	j = 0;
+	ft_bzero(&w, sizeof(w));
+	while (++i < 16)
+	{
+		w[i] = ((m[j]) << 24) | ((m[j + 1]) << 16) |\
+			((m[j + 2]) << 8) | ((m[j + 3]));
+		j += 4;
+	}
+	i = 15;
+	while (++i < 64)
+		w[i] = w[i - 16] + ((R_ROT(w[i - 15], 7)) ^ (R_ROT(w[i - 15], 18)) ^ \
+		(R_SFT(w[i - 15], 3))) + w[i - 7] + ((R_ROT(w[i - 2], 17)) ^ \
+		(R_ROT(w[i - 2], 19)) ^ (R_SFT(w[i - 2], 10)));
+	i = -1;
+	while (++i < 64)
+		hash = compute_sha256(hash, i, w);
+	hash = update_h(hash, 8);
+	return (hash);
+}
+
+t_hash	create_padding_sha256(t_hash hash)
+{
+	size_t		mod_h;
+	uint64_t	size_hash;
+
+	mod_h = hash.len_octet_h % 64;
+	size_hash = hash.len_octet_h * 8;
+	if (mod_h < 56)
+		hash.data_to_h = ft_realloc_md5(hash.data_to_h,
+							hash.len_octet_h - mod_h + 64, hash.len_octet_h);
+	else
+		hash.data_to_h = ft_realloc_md5(hash.data_to_h,
+							hash.len_octet_h - mod_h + 128, hash.len_octet_h);
+	((char *)hash.data_to_h)[hash.len_octet_h] = 0x80;
+	hash.len_octet_h += (mod_h < 56) ? (64 - mod_h) : (128 - mod_h);
+	inttochar_64(&size_hash);
+	hash.data_to_h = ft_memccat(hash.data_to_h, (void *)&size_hash,
+													(hash.len_octet_h - 8), 8);
+	return (hash);
+}
+
 void	sha256(t_hash hash)
 {
-	return ;
+	void		*ptr_save;
+	uint8_t		*hashing;
+
+	hash = create_padding_sha256(hash);
+	hash = init_variable_h_sha256(hash);
+	hashing = (uint8_t*)hash.data_to_h;
+	ptr_save = hash.data_to_h;
+	while (0 < hash.len_octet_h)
+	{
+		ft_memcpy(hash.h_update, hash.h_init, sizeof(hash.h_init));
+		hash = calculate_h_sha256(hash, hashing);
+		hash.len_octet_h -= 64;
+		hashing = hashing + 64;
+	}
+	if (ptr_save)
+		ft_memdel((void **)&ptr_save);
+	write_hash(hash);
 }
